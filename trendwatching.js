d67ffa4dfd2f3643dac16c337b3899512ef44f0e
peter kun <kuniiii@gmail.com>
2015-09-15 20:58:51 +0200

    initial image upload
    initial image upload

--- trendwatching.js
+++ trendwatching.js
@@ -1,4 +1,7 @@
 if (Meteor.isClient) {
+  $.cloudinary.config({
+    cloud_name: "trendwatching"
+  });
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -14,7 +17,26 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
-  
+
+  Template.imageUpload.helpers({
+    imageId: function () {
+      return Session.get('imageId');
+    }
+  });
+  Template.imageUpload.events({
+    "change input[type='file']": function(e) {
+      var files;
+      files = e.currentTarget.files;
+      return Cloudinary.upload(files, {
+        folder: "secret"
+      }, function(err, res) {
+        console.log("Upload Error: " + err);
+        console.log(res);
+        Session.set('imageId', res.public_id);
+      });
+    }
+  });
+
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
