668e9a020698e31f292ca7190bc766a33856a524
peter kun <kuniiii@gmail.com>
2015-09-18 12:33:13 +0200

    url validation
    url validation

--- trendwatching.js
+++ trendwatching.js
@@ -61,9 +61,17 @@ if (Meteor.isClient) {
     }
   });
 
+
   Template.imageUpload.events({
     "click button[id='uploadSubmit']": function(e) {
       console.log('button pushed!');
+      regexMatch = $('#sourceURL')[0].value.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/))
+      console.log(regexMatch);
+      if ($('#sourceURL')[0].value && regexMatch==null){
+        $('#urlTrouble').removeClass('hidden');
+        $('#urlTrouble').addClass('show');
+        return false;
+      }
       if (($('#formatted_address')[0].innerHTML)) {
         console.log('upload happening');
         //here 'show' spinner
@@ -72,13 +80,15 @@ if (Meteor.isClient) {
         files = $('#uploadFile')[0].files;
         return Cloudinary.upload(files, {
           folder: "staging",
+          folder: "shoppinglab-production",
           exif: "TRUE"
         }, function(err, res) {
             console.log("Upload Error: " + err);
             console.log(res);
-            Session.set('imageId', res.public_id); 
+            Session.set('imageId', res.public_id);
             Meteor.call('addImageEntry', 
               res.public_id, 
+              // $('#sourceURL')[0].value,
               $('#sourceURL')[0].value,
               $('#formatted_address')[0].innerHTML,
               $('#gps')[0].innerHTML,
@@ -86,8 +96,11 @@ if (Meteor.isClient) {
             $('#uploadFile')[0].value=''
             $('#sourceURL')[0].value=''
             $('#formatted_address')[0].innerHTML=''
+            $('#place')[0].value=''
             $('#gps')[0].innerHTML=''
             $('#imageComment')[0].value=''
+            $('#urlTrouble').removeClass('show');
+            $('#urlTrouble').addClass('hidden');
             //here hide spinner
           });
 
