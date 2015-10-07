7bf08e89abcdd35f4125b1faf21fe7b320aed0fd
peter kun <kuniiii@gmail.com>
2015-10-07 20:18:30 +0200

    dashboard and map for every user

--- server/server.js
+++ server/server.js
@@ -21,34 +21,54 @@ Meteor.publish("imageEntries", function(){
 Meteor.publish("allImageEntriesDB", function(){
   //returns undefined if not logged in so check if logged in first
   if(this.userId) {
-    // console.log("allImageEntriesDB "+this.userId);
+
     //var user is the same info as would be given in Meteor.user();
-    var user = Meteor.users.findOne(this.userId);
-    if (user.username == "peter" || user.username == "justien Marseille"){
-      return ImageEntries.find();
-    }
+    // var user = Meteor.users.findOne(this.userId);
+
+    // console.log("allImageEntriesDB "+this.userId);
+    return ImageEntries.find();
   }
       return null;
 });
 
-
 Meteor.publish("allUsersDB", function(){
   //console.log('call allusersdb '+this.userId);
   if (this.userId) {
-    var user = Meteor.users.findOne(this.userId);
+    // var user = Meteor.users.findOne(this.userId);
     //console.log(user.username);
 
-    if (user.username == "peter" || user.username == "justien Marseille"){
-      return Meteor.users.find({});
-    };
+      // return Meteor.users.find({});
+      return Meteor.users.find({}, {fields: {'username': 1}});
   }
   return null;
 });
 
-// Meteor.publish("collection", function() {
-//     //returns undefined if not logged in so check if logged in first
-//     if(this.userId) {
-//         var user = Meteor.users.findOne(this.userId);
-//         //var user is the same info as would be given in Meteor.user();
+
+// ### WORKING VALIDATION FOR PETER AND JUSTIEN ACCOUNTS
+
+// Meteor.publish("allImageEntriesDB", function(){
+//   //returns undefined if not logged in so check if logged in first
+//   if(this.userId) {
+//     // console.log("allImageEntriesDB "+this.userId);
+//     //var user is the same info as would be given in Meteor.user();
+//     var user = Meteor.users.findOne(this.userId);
+//     if (user.username == "peter" || user.username == "justien Marseille"){
+//       return ImageEntries.find();
 //     }
+//   }
+//       return null;
+// });
+
+
+// Meteor.publish("allUsersDB", function(){
+//   //console.log('call allusersdb '+this.userId);
+//   if (this.userId) {
+//     var user = Meteor.users.findOne(this.userId);
+//     //console.log(user.username);
+
+//     if (user.username == "peter" || user.username == "justien Marseille"){
+//       return Meteor.users.find({});
+//     };
+//   }
+//   return null;
 // });
