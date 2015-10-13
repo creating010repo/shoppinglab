7f5313e03eae8143f0ed822e139e6ba14950e5f4
peter kun <kuniiii@gmail.com>
2015-10-13 15:37:15 +0200

    map customizations

--- client/map/map.js
+++ client/map/map.js
@@ -5,7 +5,10 @@ Template.mapView.helpers({
       // Map initialization options
       return {
         center: new google.maps.LatLng(51.917937,4.487838),
-        zoom: 7
+        zoom: 7,
+        styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
+        mapTypeControl: false,
+        streetViewControl: false
       };
     }
   }
@@ -24,8 +27,16 @@ Template.mapView.onCreated(function() {
           var thisGps = thisDoc.gps.split(',');
           var latlon = new google.maps.LatLng(thisGps[0],thisGps[1]);
           // console.log(thisGps[0] + " , " + thisGps[1]);
+          var pinIcon = {
+            anchor: new google.maps.Point(11, 32),
+            origin: new google.maps.Point(0, 0),
+            scaledSize: new google.maps.Size(21, 32),
+            size: new google.maps.Size(42, 64),
+            url: 'retailinnovatie-marker.svg',
+          };          
           var marker = new google.maps.Marker({
             position: latlon,
+            icon: pinIcon,
             map: map.instance,
             id: thisDoc._id
           });
